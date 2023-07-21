3b68648f4d234e23a52bda042b65e5eb52582c6e
Brandon <32753167+btoms20@users.noreply.github.com>
2023-07-21 08:46:11 -0700

    Formatting

--- Sources/Multiaddr/Protocol Helpers/IPFS.swift
+++ Sources/Multiaddr/Protocol Helpers/IPFS.swift
@@ -5,17 +5,17 @@
 //  Modified by Brandon Toms on 5/1/22.
 //
 
+import CID
 import Foundation
 import Multihash
 import VarInt
-import CID
 
 struct P2P {
     static func data(for address: String) throws -> Data {
         let multihash = try (try? CID(address).multihash) ?? Multihash(multihash: address)
         return Data(putUVarInt(UInt64(multihash.value.count)) + multihash.value)
     }
-    
+
     static func string(for data: Data) throws -> String {
         let varInt = uVarInt(data.bytes)
         guard varInt.bytesRead + Int(varInt.value) == data.count else { throw MultiaddrError.invalidFormat }
