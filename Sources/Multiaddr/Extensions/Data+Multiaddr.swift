144b8573b29ceac814e0838c916c0931b5bcc8f7
Brandon <32753167+btoms20@users.noreply.github.com>
2025-03-01 17:07:00 -0800

    Replaced withUnsafeBytes with an optional loader instead. TODO: Returning 0 on error is probably a bad idea.

--- Sources/Multiaddr/Extensions/Data+Multiaddr.swift
+++ Sources/Multiaddr/Extensions/Data+Multiaddr.swift
@@ -19,9 +19,25 @@
 import Foundation
 
 extension Data {
+
     var uint16: UInt16 {
-        withUnsafeBytes {
-            $0.load(as: UInt16.self)
+        self.value(ofType: UInt16.self, at: 0, convertEndian: true) ?? 0
+    }
+
+    var uint32: UInt32 {
+        self.value(ofType: UInt32.self, at: 0, convertEndian: true) ?? 0
+    }
+
+    fileprivate func value<T: BinaryInteger>(ofType: T.Type, at offset: Int, convertEndian: Bool = false) -> T? {
+        let byteCount = MemoryLayout<T>.size
+        let startIndex = self.index(self.startIndex, offsetBy: offset)
+        let endIndex = self.index(startIndex, offsetBy: byteCount)
+        guard self.endIndex >= endIndex else { return nil }
+        let bytes = self[startIndex..<endIndex]
+        if convertEndian {
+            return bytes.reversed().reduce(0) { T($0) << 8 + T($1) }
+        } else {
+            return bytes.reduce(0) { T($0) << 8 + T($1) }
         }
     }
 }
